44f634f6a2d361c4a99606ae5dacbd9982a16431
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-09-25 12:37:13 +0530

    footer logo

--- wp-content/themes/synergy-theme/footer.php
+++ wp-content/themes/synergy-theme/footer.php
@@ -3,6 +3,100 @@
 Template Name: Footer
 */
 ?>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="footer-sec-1">
+                    <img src="<?php echo get_field('footer_logo');?>">
+                    <p>Sed quaerat cupiditate ut aspernatur pariatur
+                        quo facere dolores et natus quisqua.</p>
+
+                    <h5>Stay Tuned</h5>
+
+                    <ul>
+                        <li><img src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/img/x.png"></li>
+                        <li><img src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/img/yt.png"></li>
+                        <li><img src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/img/insta.png"></li>
+                        <li><img src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/img/web.png"></li>
+                    </ul>
+
+                </div>
+            </div>
+            <div class="col-md-8 offset-md-1">
+                <div class="footer-sec-2">
+                    <div class="d-flex">
+                        <h4 style="
+    color: #fff;
+">Register For Our <br>
+                            Updates!</h4>
+                        <div style="
+    width: 500px;
+    margin-left: auto;
+    position: relative;
+">
+
+                            <input type="text" style="
+    width: 100%;
+    height: 54px;
+    border-radius: 12.189px;
+    padding-left: 20px;
+" placeholder="Enter your email address">
+                            <button class="button-sub"><img src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/img/plane.svg"></button>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+
+                <div class="row quick-link">
+                    <div class="col">
+                        <h5>Support Pages</h5>
+                        <ul>
+                            <li>About</li>
+                            <li>Live Chat</li>
+                            <li>Trading Guide</li>
+                            <li>Terms & Conditions</li>
+                            <li>Privacy Policy</li>
+                            <li>Risk Disclosure</li>
+
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h5>About</h5>
+                        <ul>
+                            <li>Our Story</li>
+                            <li>Our Team</li>
+                            <li>Portfolio</li>
+                            <li>Career</li>
+                            <li>Client Testimonials</li>
+                            <li>Security Promise</li>
+
+                        </ul>
+                    </div>
+                    <div class="col">
+                        <h5>Quick Links</h5>
+                        <ul>
+                            <li>About</li>
+                            <li>Live Chat</li>
+                            <li>Trading Guide</li>
+                            <li>Terms & Conditions</li>
+                            <li>Privacy Policy</li>
+                            <li>Risk Disclosure</li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2025 senergy. All Rights Reserved</p>
+            <p>Privacy Policy | Terms & Condition</p>
+        </div>
+    </div>
+</footer>
 <script src="<?php echo get_site_url(); ?>/wp-content/themes/synergy-theme/assets/js/jquery.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
