3d4995e316c1077e9652403b26c269bc94a63d28
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-09-25 11:06:29 +0530

    design changes - remove years and style changes

--- wp-content/themes/synergy-theme/assets/js/theme-main.js
+++ wp-content/themes/synergy-theme/assets/js/theme-main.js
@@ -25,62 +25,6 @@
   function gsapController() {
     let mediaMatch = gsap.matchMedia();
 
-    // h9 service sticky
-    function initStickyAndProgress() {
-      if ($(".tj-sticky-panel-2").length > 0) {
-        let tl = gsap.timeline();
-        let panels = document.querySelectorAll(".tj-sticky-panel-2");
-        panels.forEach((panel, i) => {
-          tl.to(panel, {
-            scrollTrigger: {
-              trigger: panel,
-              pin: panel,
-              scrub: 1,
-              start: "top top",
-              end: "bottom+=120 bottom",
-              endTrigger: ".tj-sticky-panel-container-2",
-              pinSpacing: false,
-              markers: false,
-            },
-          });
-        });
-      }
-
-      // Scroll Progress animation
-      if ($(".tj-progress-item").length > 0) {
-        const tjProgressWrapper = document.querySelector(".tj-progress-wrapper");
-
-        if (tjProgressWrapper?.children?.length) {
-          let panels = gsap.utils.toArray(".tj-progress-item");
-          let totalPanels = panels.length;
-          let scrollProgressItems = gsap.utils.toArray(".tj-scroll-progress-item");
-
-          gsap.to(panels, {
-            ease: "none",
-            scrollTrigger: {
-              trigger: tjProgressWrapper,
-              start: "top top",
-              end: "bottom bottom",
-              scrub: 1,
-              pin: false,
-              onUpdate: (self) => {
-                let progress = self.progress;
-                let activeIndex = Math.round(progress * (totalPanels - 1));
-
-                panels.forEach((panel, index) => {
-                  panel.classList.toggle("active", index === activeIndex);
-                });
-                scrollProgressItems.forEach((item, index) => {
-                  item.classList.toggle("active", index === activeIndex);
-                });
-              },
-            },
-          });
-        }
-      }
-    }
-    initStickyAndProgress();
-
     // h10 process sticky
     function initStickyPanel3Animation() {
       const container = document.querySelector(".tj-sticky-panel-3-container");
@@ -105,7 +49,7 @@
               invalidateOnRefresh: true,
             },
             ease: "circ",
-            opacity: i === 0 || i === lastIdx ? 1 : 0.1,
+            opacity: i === 0 || i === lastIdx ? 1 : 0.0,
           });
         });
       });
