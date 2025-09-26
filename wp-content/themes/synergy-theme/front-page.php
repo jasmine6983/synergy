2db66831b72af00b70fe81670d8ffabf7ab18ac8
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-09-26 15:13:24 +0530

    about section color correction

--- wp-content/themes/synergy-theme/front-page.php
+++ wp-content/themes/synergy-theme/front-page.php
@@ -100,17 +100,7 @@ get_header();
     top: 45px;
     left: 30px;
 ">
-                        <!-- <svg style="    position: absolute;
-    left: 20px;
-    top: 30px;" width="76" height="34" viewBox="0 0 76 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.267578" y="0.650391" width="75.6831" height="32.4356" rx="16.2178"
-                                fill="#64BCFF" fill-opacity="0.32"></rect>
-                            <rect x="0.723501" y="1.10631" width="74.7712" height="31.5237" rx="15.7619"
-                                stroke="#64BCFF" stroke-opacity="0.37" stroke-width="0.911846"></rect>
-                            <path
-                                d="M28.3391 10.9401L24.5814 18.1778V22.2949H22.7189V18.1778L18.9448 10.9401H21.0197L23.6501 16.5113L26.2806 10.9401H28.3391ZM31.6797 12.4432V15.7761H35.6008V17.2955H31.6797V20.7755H36.091V22.2949H29.8172V10.9237H36.091V12.4432H31.6797ZM45.2306 19.9749H40.4763L39.6594 22.2949H37.7152L41.7833 10.9237H43.9399L48.0081 22.2949H46.0475L45.2306 19.9749ZM44.7078 18.4555L42.8616 13.1784L40.9991 18.4555H44.7078ZM55.6749 22.2949L53.0608 17.753H51.6394V22.2949H49.7769V10.9401H53.698C54.5694 10.9401 55.3046 11.0925 55.9036 11.3975C56.5136 11.7025 56.9656 12.1109 57.2597 12.6229C57.5647 13.1348 57.7172 13.7066 57.7172 14.3384C57.7172 15.079 57.4993 15.7543 57.0636 16.3643C56.6388 16.9633 55.9799 17.3718 55.0867 17.5896L57.8969 22.2949H55.6749ZM51.6394 16.2662H53.698C54.3951 16.2662 54.9179 16.092 55.2665 15.7434C55.6259 15.3949 55.8056 14.9265 55.8056 14.3384C55.8056 13.7502 55.6313 13.2927 55.2828 12.966C54.9343 12.6283 54.406 12.4595 53.698 12.4595H51.6394V16.2662Z"
-                                fill="white"></path>
-                        </svg> -->
+                        
                         <h2 style="
     margin-top: 100px;
     color: #65BCFF;
@@ -166,6 +156,8 @@ get_header();
                 // Captions
                 $caption_1 = $about['caption_1'] ?? 'no data';
                 $caption_2 = $about['caption_2'] ?? 'no data';
+                $caption_3 = $about['caption_3'] ?? 'no data';
+                
 
 
                 // Descriptions
@@ -191,7 +183,7 @@ get_header();
                 $about_right_image = $about['about_right_image'];
                 ?>
 
-                <h1> <span><?php echo $caption_1; ?></span> <?php echo $caption_2; ?></h1>
+                <h1><?php echo $caption_1; ?> <span><?php echo $caption_2; ?></span> <?php echo $caption_3; ?> </h1>
                 <p> <?php echo $description_1; ?></p>
 
                 <p> <?php echo $description_2; ?> </p>
