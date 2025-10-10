94b930de2ffd6da4039435e755de5c62a334ff6d
jasmine6983 <jasmine.m@techversantinfotech.com>
2025-10-10 13:23:03 +0530

    change email id to test email

--- wp-content/plugins/custom-contact-form/custom-contact-form.php
+++ wp-content/plugins/custom-contact-form/custom-contact-form.php
@@ -65,7 +65,7 @@ function synergy_handle_contact_form()
     ]);
 
     // Send Email
-    $to = 'contact@synergyaiwealth.com';
+    $to = 'jasmine.m@techversantinfotech.com';
     $subject = "New Contact Form Submission";
     $body = "You received a new message:\n\n" .
         "Name: $first_name $last_name\n" .
